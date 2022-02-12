63e8271e373d5451d5f9cdabf0903a50fc5f2321
Jesse Walter Vanderwerf <jessev@global.id>
2022-02-11 17:40:55 -0800

    installed nestjs/axios, updated get to use nestjs/axios, stubbed out test

--- globalid-connect/node/src/verifications/identity/identity.service.spec.ts
+++ globalid-connect/node/src/verifications/identity/identity.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { IdentityService } from './identity.service';
 
 describe('IdentityService', () => {
@@ -6,7 +7,7 @@ describe('IdentityService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IdentityService],
+      providers: [IdentityService]
     }).compile();
 
     service = module.get<IdentityService>(IdentityService);
@@ -14,5 +15,16 @@ describe('IdentityService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('get', () => {
+    it("should return user's identity", async () => {
+      const code = 'blah';
+      const identity = {};
+
+      const result = await service.get(code);
+
+      expect(result).toBe(identity);
+    });
   });
 });
--- globalid-connect/node/src/verifications/identity/identity.service.ts
+++ globalid-connect/node/src/verifications/identity/identity.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { HttpService } from '@nestjs/axios';
+import { HttpException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
-export class IdentityService {}
+export class IdentityService {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+    private httpService: HttpService
+  ) {}
+
+  private get redirectUri() {
+    return this.configService.get<string>('IDENTITY_REDIRECT_URI');
+  }
+
+  async get(code: string) {
+    const { access_token } = await this.authService.getTokens({ code, redirectUri: this.redirectUri });
+
+    const response = lastValueFrom(
+      this.httpService
+        .get('https://api.global.id/v1/identity/me', { headers: { Authorization: `Bearer ${access_token}` } })
+        .pipe(
+          map((response) => {
+            return response.data;
+          }),
+          catchError((e) => {
+            throw new HttpException(e.response.data, e.response.status);
+          })
+        )
+    );
+
+    return response;
+  }
+}
