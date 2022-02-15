041892104952566d02d44c60d20b762e7101ef44
Robert Parker <robert@global.id>
2022-02-15 10:00:53 -0800

    DEVOFF-40 remove identity for now, all tests passing

--- globalid-connect/node/src/verifications/verifications.module.ts
+++ globalid-connect/node/src/verifications/verifications.module.ts
@@ -5,13 +5,12 @@ import { PiiController } from './pii/pii.controller';
 import { PiiService } from './pii/pii.service';
 import { VaultService } from './vault/vault.service';
 import { VerificationsController } from './verifications.controller';
-import { IdentityService } from './identity/identity.service';
 import { AttestationsService } from './attestations/attestations.service';
 import { AttestationsController } from './attestations/attestations.controller';
 
 @Module({
   imports: [HttpModule],
   controllers: [AttestationsController, PiiController, VerificationsController],
-  providers: [AttestationsService, AuthService, PiiService, VaultService, IdentityService]
+  providers: [AttestationsService, AuthService, PiiService, VaultService]
 })
 export class VerificationsModule {}
