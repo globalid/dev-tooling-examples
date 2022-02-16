b4656acc566c2f6a7191d7438581d0b3e70c6eee
Jesse Vanderwerf <jessev@global.id>
2022-02-15 17:00:47 -0800

    added identity controller

---
+++ globalid-connect/node/src/verifications/identity/identity.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { IdentityService } from './identity.service';
+
+@Controller('identity')
+export class IdentityController {
+    constructor(private readonly identity: IdentityService) {}
+
+    @Get('connect/identity')
+    getIdentity(@Query('code') code: string) {
+      return this.identity.getIdentity(code);
+    }
+}
--- globalid-connect/node/src/verifications/verifications.module.ts
+++ globalid-connect/node/src/verifications/verifications.module.ts
@@ -7,10 +7,12 @@ import { PiiService } from './pii/pii.service';
 import { VaultService } from './vault/vault.service';
 import { VerificationsController } from './verifications.controller';
 import { IdentityService } from './identity/identity.service';
+import { IdentityController } from './identity/identity/identity.controller';
+import { IdentityController } from './identity/identity.controller';
 
 @Module({
   imports: [HttpModule],
-  controllers: [PiiController, VerificationsController],
+  controllers: [PiiController, VerificationsController, IdentityController],
   providers: [AuthService, PiiService, VaultService, IdentityService]
 })
 export class VerificationsModule {}
