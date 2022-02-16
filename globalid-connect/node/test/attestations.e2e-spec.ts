6b8d0883872fe9372a9fcdda89512cd9ac95e42b
Robert Parker <robert@global.id>
2022-02-16 14:54:50 -0800

    DEVOFF-48 add e2e test for Attestations. Gets stuck on request.
    DEVOFF-48 finish attestations e2e test

---
+++ globalid-connect/node/test/attestations.e2e-spec.ts
@@ -0,0 +1,42 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { AttestationsService } from '../src/verifications/attestations/attestations.service'
+import { INestApplication } from '@nestjs/common';
+import { VerificationsModule } from '../src/verifications/verifications.module';
+import { createMock } from '@golevelup/ts-jest';
+import { Attestation } from '../src/verifications/attestations/attestation.interface';
+import { ConfigService } from '@nestjs/config';
+
+describe('Attestations', () => {
+  let app: INestApplication;
+  let attestationsMock: Attestation[];
+  let attestationsService: Partial<AttestationsService>;
+
+  beforeAll(async () => {
+    attestationsMock = createMock<Attestation[]>();
+    attestationsService = {
+      getAttestations: async (code: string) => attestationsMock,
+    };
+    const moduleRef = await Test.createTestingModule({
+      imports: [VerificationsModule]
+    })
+      .overrideProvider(AttestationsService)
+      .useValue(attestationsService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`GET /verifications/connect/attestations`, () => {
+    request(app.getHttpServer())
+      .get('/verifications/connect/attestations')
+      .query({ code: '123' })
+      .expect(200)
+      .then(resp => {
+        expect(resp.body).toMatchObject({
+            data: attestationsMock,
+          })
+      });
+  });
+});
