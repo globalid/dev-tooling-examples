4317b9da6d847059dd3996c0775e3092a1135335
Robert Parker <robert@global.id>
2022-02-22 10:37:40 -0800

    DEVOFF-74 move jest config, add missing coverage, all necessary tests covered/passed

---
+++ globalid-connect/node/jest.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    },
+    'src/{**/*.module.ts,main.ts}': {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0
+    }
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  verbose: true
+};
+
+export default config;
