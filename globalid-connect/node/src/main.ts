31a08b7cb47a1561c1f4e079d29ee8eccf6a2147
Robert Parker <robert@global.id>
2022-02-25 10:46:51 -0800

    DEVOFF-78 remove environment specific logger settings

--- globalid-connect/node/src/main.ts
+++ globalid-connect/node/src/main.ts
@@ -6,10 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger:
-      process.env.NODE_ENV === 'development' ? ['verbose', 'debug', 'log', 'warn', 'error'] : ['log', 'warn', 'error']
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
