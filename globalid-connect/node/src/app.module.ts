6165266f3d2e677039d30aeb5cd77a7863dd1aa6
Robert Parker <robert@global.id>
2022-02-25 12:35:57 -0800

    DEVOFF-78 review cleanup

--- globalid-connect/node/src/app.module.ts
+++ globalid-connect/node/src/app.module.ts
@@ -2,15 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
-import config, { validationSchema } from './config';
+import { validationSchema } from './config';
 import { VerificationsModule } from './verifications/verifications.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [config],
-      validationSchema: validationSchema
+      validationSchema
     }),
     VerificationsModule
   ],
---
+++ globalid-connect/node/src/config.schema.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string(),
+  CLIENT_ID: Joi.string(),
+  CLIENT_SECRET: Joi.string(),
+  ATTESTATIONS_CONNECT_URL: Joi.string().uri(),
+  ATTESTATIONS_REDIRECT_URI: Joi.string().uri(),
+  IDENTITY_CONNECT_URL: Joi.string().uri(),
+  IDENTITY_REDIRECT_URI: Joi.string().uri(),
+  PII_CONNECT_URL: Joi.string().uri(),
+  PII_REDIRECT_URI: Joi.string().uri(),
+  PRIVATE_KEY: Joi.string(),
+  PRIVATE_KEY_PASSPHRASE: Joi.string()
+});
