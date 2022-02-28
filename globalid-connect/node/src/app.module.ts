c12a053236f9d16780604fdf40b325680b3e0b59
robertgid <98610918+robertgid@users.noreply.github.com>
2022-02-28 07:22:50 -0800

    DEVOFF-78: Validating Configuration (#24)

--- globalid-connect/node/src/app.module.ts
+++ globalid-connect/node/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
+import validationSchema from './config.schema';
 import { VerificationsModule } from './verifications/verifications.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
+      validationSchema
     }),
     VerificationsModule
   ],
