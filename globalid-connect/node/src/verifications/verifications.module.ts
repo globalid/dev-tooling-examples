0d3cf7882bdcebf635fda6a2d879ec9fd83e92db
Jesse Vanderwerf <jessev@global.id>
2022-03-10 14:10:37 -0800

    fix(DEVOFF-102): correcting closing tags

--- globalid-connect/node/src/verifications/verifications.module.ts
+++ globalid-connect/node/src/verifications/verifications.module.ts
@@ -3,13 +3,12 @@ import { Module } from '@nestjs/common';
 
 import { gidApiClientFactoryProvider } from './gid-api-client.factory';
 import { NonceService } from './nonce.service';
-import { ResultsController } from './results/results.controller';
 import { VerificationsController } from './verifications.controller';
 import { VerificationsService } from './verifications.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [VerificationsController, ResultsController],
-  providers: [NonceService, VerificationsService, gidApiClientFactoryProvider]
+  controllers: [VerificationsController],
+  providers: [gidApiClientFactoryProvider, NonceService, VerificationsService]
 })
 export class VerificationsModule {}
