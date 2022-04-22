33216cdb3e05cef09ef6c89d0af57040a5c89343
Jesse Vanderwerf <jessev@global.id>
2022-04-22 12:24:28 -0700

    IDOPS-976: code style changes

--- verifier-agent/node/src/gid/create-presentation-request-url.ts
+++ verifier-agent/node/src/gid/create-presentation-request-url.ts
@@ -8,15 +8,25 @@ export interface CreatePresentationUrlParams {
 export const createPresentationRequestUrl = (params: CreatePresentationUrlParams): URL => {
   const { trackingId, clientId, initiationUrl, redirectUrl } = params;
 
-  const linkUrl = new URL('https://link.global.id');
-  linkUrl.searchParams.append('app_uuid', clientId);
-  const proofRequestUrl = new URL(initiationUrl);
-  proofRequestUrl.searchParams.append('tracking_id', trackingId);
+  const linkUrl = urlWithParams('https://link.global.id', { app_uuid: clientId });
+  const proofRequestUrl = urlWithParams(initiationUrl, { tracking_id: trackingId });
+
   linkUrl.searchParams.append('proof_request_url', proofRequestUrl.toString());
-  if (redirectUrl !== undefined) {
-    const redirect = new URL(redirectUrl);
-    redirect.searchParams.append('tracking_id', trackingId);
+
+  if (redirectUrl) {
+    const redirect = urlWithParams(redirectUrl, { tracking_id: trackingId });
     linkUrl.searchParams.append('redirect_url', redirect.toString());
   }
+
   return linkUrl;
+};
+
+const urlWithParams = (uri: string, searchParams: Record<string, string>): URL => {
+  const url = new URL(uri);
+
+  Object.keys(searchParams).forEach((key: string) => {
+    url.searchParams.append(key, searchParams[key]);
+  });
+
+  return url;
 };
