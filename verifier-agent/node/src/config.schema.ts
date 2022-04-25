05a55eee2845a7448c07a61ae8f3e11ded01442a
Robert Parker <robert@global.id>
2022-04-25 13:40:18 -0700

    IDOPS-954 add BASE_URL to .env and validationSchema

--- verifier-agent/node/src/config.schema.ts
+++ verifier-agent/node/src/config.schema.ts
@@ -3,6 +3,7 @@ import * as Joi from 'joi';
 export default Joi.object({
   CLIENT_ID: Joi.string(),
   CLIENT_SECRET: Joi.string(),
+  BASE_URL: Joi.string().uri(),
   GID_CREDENTIALS_BASE_URL: Joi.string().uri(),
   GID_API_BASE_URL: Joi.string().uri()
 });
