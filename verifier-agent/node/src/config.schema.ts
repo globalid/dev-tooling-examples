d1c2a180c9e5a68cd63f5ccd023f5ad2ea3a4e96
Robert Parker <robert@global.id>
2022-04-26 11:57:13 -0700

    IDOPS-953 all env vars required

--- verifier-agent/node/src/config.schema.ts
+++ verifier-agent/node/src/config.schema.ts
@@ -1,9 +1,9 @@
 import * as Joi from 'joi';
 
 export default Joi.object({
-  CLIENT_ID: Joi.string(),
-  CLIENT_SECRET: Joi.string(),
-  BASE_URL: Joi.string().uri(),
-  GID_CREDENTIALS_BASE_URL: Joi.string().uri(),
-  GID_API_BASE_URL: Joi.string().uri()
+  CLIENT_ID: Joi.string().required(),
+  CLIENT_SECRET: Joi.string().required(),
+  BASE_URL: Joi.string().uri().required(),
+  GID_CREDENTIALS_BASE_URL: Joi.string().uri().required(),
+  GID_API_BASE_URL: Joi.string().uri().required()
 });
