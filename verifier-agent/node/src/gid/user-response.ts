2de6c0ceecb935762a2845ac8ffae539f799e011
Robert Parker <robert@global.id>
2022-04-28 10:39:54 -0700

    IDOPS-953 Add validation decorators

--- verifier-agent/node/src/gid/user-response.ts
+++ verifier-agent/node/src/gid/user-response.ts
@@ -1,24 +1,30 @@
-import { IsUUID } from 'class-validator';
+import { IsBoolean, IsEnum, IsString, IsUUID, ValidateNested } from 'class-validator';
 import { VerifiablePresentation } from '../presentation-request/presentation-request.types';
-
-abstract class UserResponse {
-  @IsUUID('4')
-  app_uuid: string;
-  tracking_id: string;
-  thread_id: string;
-  state: UserResponseState;
-  verified: boolean;
-}
 
 export enum UserResponseState {
   Abandoned = 'abandoned',
   Done = 'done'
 }
 
+abstract class UserResponse {
+  @IsUUID('4')
+  app_uuid: string;
+  @IsString()
+  tracking_id: string;
+  @IsString()
+  thread_id: string;
+  @IsEnum(UserResponseState)
+  state: UserResponseState;
+  @IsBoolean()
+  verified: boolean;
+}
+
 export class UserAcceptance extends UserResponse {
+  @ValidateNested()
   proof_presentation: VerifiablePresentation;
 }
 
 export class UserRejection extends UserResponse {
+  @IsString()
   error_msg: string;
 }
