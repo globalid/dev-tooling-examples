36d3515a1be0383b3b023ea4812b021ea5090092
Brad Stell <brad@global.id>
2022-07-13 15:56:14 -0400

    IDOPS-1135 adding in presentation request object requiring pre-reqs for email, phone number and self declared

--- verifier-agent/node/src/presentation-request/presentation-requirements.factory.ts
+++ verifier-agent/node/src/presentation-request/presentation-requirements.factory.ts
@@ -1,9 +1,5 @@
-import * as dayjs from 'dayjs';
-
 import {
   AllowanceStatus,
-  FilterValueFormat,
-  FilterValueType,
   PresentationRequirements,
   ProofAlgorithm,
   RequiredStatus,
@@ -16,8 +12,8 @@ export class PresentationRequirementsFactory {
   create(): PresentationRequirements {
     return {
       name: 'Proof Requirements',
-      purpose: 'To demonstrate creating the Presentation Requirements for a Presentation Request',
-      id: 'c4272baf-1c96-4246-95e2-ed816f471793',
+      purpose: 'To demonstrate creating the Presentation Requirements for a Presentation Request with prerequisites',
+      id: '0748bed6-ffb3-4f68-a3e2-fd4362b63c24',
       format: {
         ldp_vp: {
           proof_type: [ProofAlgorithm.BbsBlsSignature2020]
@@ -25,46 +21,131 @@ export class PresentationRequirementsFactory {
       },
       input_descriptors: [
         {
-          id: 'government_id',
-          name: 'Government ID',
-          schema: {
-            oneof_filter: [
-              {
-                required: true,
-                uri: 'https://credentials.global.id/v1/schema-registry/contexts/Driving%20license/versions/1#Driving%20license'
-              },
-              {
-                required: true,
-                uri: 'https://credentials.global.id/v1/schema-registry/contexts/Passport/versions/2#Passport'
-              }
-            ]
-          },
+          id: 'phone_number',
+          name: 'Phone Number',
+          schema: [
+            {
+              required: true,
+              uri: 'https://credentials.global.id/v1/schema-registry/contexts/Phone%20number/versions/1'
+            }
+          ],
           constraints: {
             limit_disclosure: RequirementStatus.Required,
             is_holder: [
               {
                 directive: 'required',
-                field_id: ['1f44d55f-f161-4938-a659-f8026467f126']
+                field_id: ['ff6844b1-5597-4de6-9910-df64721a0519']
               }
             ],
             status_active: AllowanceStatus.Allowed,
-            status_revoked: AllowanceStatus.Allowed,
-            status_suspended: RequiredStatus.Allowed,
+            status_revoked: AllowanceStatus.Disallowed,
+            status_suspended: RequiredStatus.Disallowed,
             subject_is_issuer: RequirementStatus.Preferred,
             fields: [
               {
-                id: '1f44d55f-f161-4938-a659-f8026467f126',
-                path: ['$.credentialSubject.date_of_birth'],
-                purpose: 'Holder must be over 18',
-                filter: {
-                  maximum: dayjs().subtract(18, 'years').format('YYYY-MM-DD'),
-                  type: FilterValueType.String,
-                  format: FilterValueFormat.Date
-                }
+                id: 'ff6844b1-5597-4de6-9910-df64721a0519',
+                path: ['$.credentialSubject.phone_number'],
+                purpose: 'Holder must have a valid phone number',
               }
             ]
           }
-        }
+        },
+        {
+          id: 'email',
+          name: 'Email',
+          schema: [
+            {
+              required: true,
+              uri: 'https://credentials.global.id/v1/schema-registry/contexts/Email/versions/1'
+            }
+          ],
+          constraints: {
+            limit_disclosure: RequirementStatus.Required,
+            is_holder: [
+              {
+                directive: 'required',
+                field_id: ['b48fabf6-9e99-48a5-9a89-bd58c7b7e9b3']
+              }
+            ],
+            status_active: AllowanceStatus.Allowed,
+            status_revoked: AllowanceStatus.Disallowed,
+            status_suspended: RequiredStatus.Disallowed,
+            subject_is_issuer: RequirementStatus.Preferred,
+            fields: [
+              {
+                id: 'b48fabf6-9e99-48a5-9a89-bd58c7b7e9b3',
+                path: ['$.credentialSubject.email'],
+                purpose: 'Holder must have an email address',
+              }
+            ]
+          }
+        },
+        {
+          id: 'self_declared_id',
+          name: 'Self Declared ID',
+          schema: [
+            {
+              required: true,
+              uri: 'https://credentials.global.id/v1/schema-registry/contexts/Identity%20Declaration/versions/1'
+            }
+          ],
+          constraints: {
+            limit_disclosure: RequirementStatus.Required,
+            is_holder: [
+              {
+                directive: 'required',
+                field_id: ['86801c4d-0e4e-4811-9af3-c72b5cd75219']
+              },
+              {
+                directive: 'required',
+                field_id: ['df950bbe-5855-44b8-8bfb-395ca792890c']
+              },
+              {
+                directive: 'required',
+                field_id: ['421f1366-335d-43fc-bfa4-c6bc6455be05']
+              },
+              {
+                directive: 'required',
+                field_id: ['6a55adab-e941-4e32-8324-3c03c0a11902']
+              },
+              {
+                directive: 'required',
+                field_id: ['275536cf-f104-4430-b53a-c7af5cf76108']
+              },
+            ],
+            status_active: AllowanceStatus.Allowed,
+            status_revoked: AllowanceStatus.Disallowed,
+            status_suspended: RequiredStatus.Disallowed,
+            subject_is_issuer: RequirementStatus.Preferred,
+            fields: [
+              {
+                id: '86801c4d-0e4e-4811-9af3-c72b5cd75219',
+                path: ['$.credentialSubject.address_full'],
+                purpose: 'The full address of the credential holder',
+              },
+              {
+                id: 'df950bbe-5855-44b8-8bfb-395ca792890c',
+                path: ['$.credentialSubject.ip_address'],
+                purpose: 'The ip address of the credential holder',
+              },
+              {
+                id: '421f1366-335d-43fc-bfa4-c6bc6455be05',
+                path: ['$.credentialSubject.address_country'],
+                purpose: 'The Country of residence of the credential holder',
+              },
+              {
+                id: '6a55adab-e941-4e32-8324-3c03c0a11902',
+                path: ['$.credentialSubject.date_of_birth'],
+                purpose: 'The date of birth of the credential holder',
+              },
+              {
+                id: '275536cf-f104-4430-b53a-c7af5cf76108',
+                path: ['$.credentialSubject.full_name_legal'],
+                purpose: 'The full legal name of the credential holder',
+              },
+            ]
+          }
+        },
       ]
     };
   }
