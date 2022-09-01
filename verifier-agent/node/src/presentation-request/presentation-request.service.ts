8fb8631a868c429bbfe73345d63e0a2e29b02d8a
imponenm <marcus.imponenti@gmail.com>
2022-08-31 18:06:26 -0700

    Added back-end code to parse and post data to Janusea

--- verifier-agent/node/src/presentation-request/presentation-request.service.ts
+++ verifier-agent/node/src/presentation-request/presentation-request.service.ts
@@ -13,6 +13,8 @@ import { InvalidSignatureError } from './invalid-signature.error';
 import { PresentationRequirementsFactory } from './presentation-requirements.factory';
 import { QrCodeViewModel } from './qr-code.view-model';
 
+const axios = require('axios');
+
 @Injectable()
 export class PresentationRequestService {
   private readonly logger = new Logger(PresentationRequestService.name);
@@ -45,6 +47,25 @@ export class PresentationRequestService {
     });
   }
 
+  async postToJanusea(holderResponse: HolderAcceptance) {
+    // Parse the data and re-name keys to match the Janusea API
+    const data = holderResponse.proofPresentation.dif.verifiableCredential[0].credentialSubject;
+    delete data['id']; // ID of the proof request, which isn't needed
+    Object.defineProperty(data, "full_legal_name", Object.getOwnPropertyDescriptor(data, "full_name_legal")); // Rename full_name_legal to full_legal_name
+    Object.defineProperty(data, "email_address", Object.getOwnPropertyDescriptor(data, "email")); // Rename email to email_address
+    Object.defineProperty(data, "full_residence_address", Object.getOwnPropertyDescriptor(data, "address_full")); // Rename full_address to full_residence_address
+    this.logger.log(data);
+
+    // Send the post request to the Janusea API
+    // axios.post('https://eco.kivagroup.com/bonifii/membership', data).then(res => {
+    //   this.logger.log(`statusCode: ${res.status}`);
+    //   this.logger.log(res.data);
+    // }).catch(err => {
+    //   this.logger.log("Request error");
+    //   this.logger.log(err);
+    // });
+  }
+
   async handleResponse(signature: string, holderResponse: HolderAcceptance | HolderRejection) {
     this.logger.log('verifying signature');
     await this.verifySignature(signature, holderResponse);
@@ -56,6 +77,7 @@ export class PresentationRequestService {
        * Janusea API call goes here. The payload is in holderResponse.proofPresentation
        *****************************************/
       // this.logger.log(holderResponse.proofPresentation);
+      this.postToJanusea(holderResponse);
     } else {
       this.logger.log('holder rejected');
       this.clientService.sendRejection(holderResponse);
