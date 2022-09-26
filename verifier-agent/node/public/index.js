e583aec430ef5845515b9db3daf3c5c8ccb8cd09
imponenm <marcus.imponenti@gmail.com>
2022-09-26 15:15:24 -0700

    Created first mobile page

--- verifier-agent/node/public/index.js
+++ verifier-agent/node/public/index.js
@@ -1,5 +1,54 @@
 'use strict';
 
+
+window.mobileCheck = function() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
+
+function displayMobilePage() {
+  // Remove the box containers
+  const top_right_box = document.getElementById('box2-top');
+  top_right_box.remove();
+
+  const bottom_containers = document.getElementById('bottom-container');
+  bottom_containers.remove();
+
+  const top_container = document.getElementById('top-container');
+  top_container.style.backgroundImage = 'none';
+
+  const box_1 = document.getElementById('box1-top');
+  box_1.style.width = '100%';
+
+  const earn25 = document.getElementById('earn25');
+  earn25.style.fontSize = '55px';
+  earn25.style.maxWidth = '85%';
+
+  const primary_description = document.getElementById('primary-description');
+  // primary_description.style.fontSize = '24px';
+  // primary_description.style.maxWidth = '85%';
+  primary_description.remove();
+
+  let desktop_notice = document.createElement('section');
+  desktop_notice.id = 'desktop-notice';
+  desktop_notice.innerHTML = 'Please use a desktop computer to access this page...';
+  desktop_notice.style.fontSize = '20px';
+  desktop_notice.style.fontFamily = 'Helvetica';
+  desktop_notice.style.color = 'white';
+  desktop_notice.style.fontWeight = '500';
+  desktop_notice.style.lineHeight = '38px';
+  desktop_notice.style.maxWidth = '85%';
+
+  let credential_description_box = document.getElementById('credential-description-box');
+  credential_description_box.append(desktop_notice);
+
+  const footer = document.getElementById('footer');
+  footer.remove();
+}
+
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function initWebSocket(url, trackingId) {
   const socket = io(url, { transports: ['websocket'] });
@@ -182,6 +231,12 @@ function createMessageArea(title, message, messageClass) {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function generateStyledQrCode(dataUrl) {
+  if(window.mobileCheck()) {
+    console.log("Mobile device detected");
+    displayMobilePage();
+    return
+  } 
+
   const qrCode = new QRCodeStyling({
     width: 250,
     height: 250,
