390675bfcb6a44dbfdc9280b0872ad7613f9ef7d
antonSvetin <anton@global.id>
2023-07-14 10:31:25 +0200

    feat(deploy): Adding config option for port of a service

--- verifier-agent/node/src/main.ts
+++ verifier-agent/node/src/main.ts
@@ -8,7 +8,7 @@ import { setup } from './setup';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   setup(app);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
